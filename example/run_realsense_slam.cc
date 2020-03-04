9349efc57ac7ecd185e6c59ef7995dfab9fab413
Jesse Anttila J J <35033029+jesseanttila-cai@users.noreply.github.com>
2020-03-04 15:19:48 +0200

    Add monocular T265 fisheye + WIP stereo

--- example/run_realsense_slam.cc
+++ example/run_realsense_slam.cc
@@ -29,6 +29,249 @@
 #include <gperftools/profiler.h>
 #endif
 
+void mono_tracking(const std::shared_ptr<openvslam::config>& cfg,
+                   const std::string& vocab_file_path,
+                   const float scale, const std::string& map_db_path) {
+    cv::setUseOptimized( true );
+
+    rs2::config config;
+
+    rs2::pipeline pipeline;
+    config.enable_stream(RS2_STREAM_FISHEYE, 0);
+    config.enable_stream(RS2_STREAM_FISHEYE, 1);
+
+    rs2::pipeline_profile pipeline_profile = pipeline.start( );//config );
+
+    // build a SLAM system
+    openvslam::system SLAM(cfg, vocab_file_path);
+    // startup the SLAM process
+    SLAM.startup(false);
+
+    if (!map_db_path.empty()) {
+        // load the map database
+        try {
+            SLAM.load_map_database(map_db_path, true);
+        } catch (...) {
+        }
+    }
+
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+#ifdef USE_PANGOLIN_VIEWER
+    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#elif USE_SOCKET_PUBLISHER
+    socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#endif
+
+    rs2::frameset frameset;
+
+    cv::Mat fisheye_frame;
+    cv::Mat mask = {};
+
+    double timestamp = 0.0;
+    std::vector<double> track_times;
+
+    unsigned int num_frame = 0;
+
+    unsigned int frame_id = 0;
+
+    // run the SLAM in another thread
+    std::thread thread([&]() {
+        while (true) {
+            // check if the termination of SLAM system is requested or not
+            if (SLAM.terminate_is_requested()) {
+                break;
+            }
+
+            frameset = pipeline.wait_for_frames();
+
+            if (frame_id == frameset.get_fisheye_frame().get_frame_number()) {
+                continue;
+            };
+            frame_id = frameset.get_fisheye_frame().get_frame_number();
+
+            fisheye_frame = cv::Mat(800, 848, CV_8U, const_cast<void*>(frameset.get_fisheye_frame().get_data()));
+
+            timestamp = frameset.get_fisheye_frame().get_timestamp() * 0.001;
+                
+            if (scale != 1.0) {
+                cv::resize(fisheye_frame, fisheye_frame, cv::Size(), scale, scale, cv::INTER_LINEAR);
+            }
+
+            const auto tp_1 = std::chrono::steady_clock::now();
+
+            // input the current frame and estimate the camera pose
+            SLAM.feed_monocular_frame(fisheye_frame, timestamp, mask);
+
+            const auto tp_2 = std::chrono::steady_clock::now();
+
+            const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+            track_times.push_back(track_time);
+
+            //timestamp += 1.0 / cfg->camera_->fps_;
+            ++num_frame;
+        }
+
+        // wait until the loop BA is finished
+        while (SLAM.loop_BA_is_running()) {
+            std::this_thread::sleep_for(std::chrono::microseconds(5000));
+        }
+    });
+
+    // run the viewer in the current thread
+    #ifdef USE_PANGOLIN_VIEWER
+    viewer.run();
+    #elif USE_SOCKET_PUBLISHER
+    publisher.run();
+    #endif
+
+    thread.join();
+
+    // shutdown the SLAM process
+    SLAM.shutdown();
+
+    if (!map_db_path.empty()) {
+        // output the map database
+        SLAM.save_map_database(map_db_path, true);
+    }
+
+    std::sort(track_times.begin(), track_times.end());
+    const auto total_track_time = std::accumulate(track_times.begin(), track_times.end(), 0.0);
+    std::cout << "median tracking time: " << track_times.at(track_times.size() / 2) << "[s]" << std::endl;
+    std::cout << "mean tracking time: " << total_track_time / track_times.size() << "[s]" << std::endl;
+
+}
+
+void stereo_tracking(const std::shared_ptr<openvslam::config>& cfg,
+                   const std::string& vocab_file_path,
+                   const float scale, const std::string& map_db_path) {
+    cv::setUseOptimized( true );
+
+    rs2::config config;
+
+    rs2::pipeline pipeline;
+    config.enable_stream(RS2_STREAM_FISHEYE, 0);
+    config.enable_stream(RS2_STREAM_FISHEYE, 1);
+
+    rs2::pipeline_profile pipeline_profile = pipeline.start( );//config );
+
+    // build a SLAM system
+    openvslam::system SLAM(cfg, vocab_file_path);
+    // startup the SLAM process
+    SLAM.startup(false);
+
+    if (!map_db_path.empty()) {
+        // load the map database
+        try {
+            SLAM.load_map_database(map_db_path, true);
+        } catch (...) {
+        }
+    }
+
+    // create a viewer object
+    // and pass the frame_publisher and the map_publisher
+#ifdef USE_PANGOLIN_VIEWER
+    pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#elif USE_SOCKET_PUBLISHER
+    socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
+#endif
+
+    rs2::frameset frameset;
+
+    cv::Mat fisheye_frame_left;
+    cv::Mat fisheye_frame_right;
+    cv::Mat mask = {};
+    
+    float camera_params[9] = {285.947, 0.0, 428.915,
+                              0.0, 285.914, 403.695,
+                              0.0, 0.0, 1.0};
+    cv::Mat camera_matrix = cv::Mat(3, 3, CV_64F, camera_params);
+    cv::Mat dummy_image = cv::Mat(848, 800, CV_8U);
+    
+    float distortion_params[4] = {-0.0125516, 0.0515311, -0.0482535, 0.00975901};
+    cv::Mat distortion_matrix = cv::Mat(4, 1, CV_64F, camera_params);
+    
+    cv::Mat dummy_out;
+    cv::fisheye::undistortImage()
+    
+    
+    
+    double timestamp = 0.0;
+    std::vector<double> track_times;
+
+    unsigned int num_frame = 0;
+
+    unsigned int frame_id = 0;
+
+    // run the SLAM in another thread
+    std::thread thread([&]() {
+        while (true) {
+            // check if the termination of SLAM system is requested or not
+            if (SLAM.terminate_is_requested()) {
+                break;
+            }
+
+            frameset = pipeline.wait_for_frames();
+
+            if (frame_id == frameset.get_fisheye_frame().get_frame_number()) {
+                continue;
+            };
+            frame_id = frameset.get_fisheye_frame().get_frame_number();
+
+            fisheye_frame_left = cv::Mat(800, 848, CV_8U, const_cast<void*>(frameset.get_fisheye_frame(1).get_data()));
+            fisheye_frame_right = cv::Mat(800, 848, CV_8U, const_cast<void*>(frameset.get_fisheye_frame(0).get_data()));
+
+            timestamp = frameset.get_fisheye_frame().get_timestamp() * 0.001;
+                
+            if (scale != 1.0) {
+                cv::resize(fisheye_frame_left, fisheye_frame_left, cv::Size(), scale, scale, cv::INTER_LINEAR);
+                cv::resize(fisheye_frame_right, fisheye_frame_right, cv::Size(), scale, scale, cv::INTER_LINEAR);
+            }
+
+            const auto tp_1 = std::chrono::steady_clock::now();
+
+            // input the current frame and estimate the camera pose
+            SLAM.feed_stereo_frame(fisheye_frame_left, fisheye_frame_right, timestamp, mask);
+
+            const auto tp_2 = std::chrono::steady_clock::now();
+
+            const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
+            track_times.push_back(track_time);
+
+            //timestamp += 1.0 / cfg->camera_->fps_;
+            ++num_frame;
+        }
+
+        // wait until the loop BA is finished
+        while (SLAM.loop_BA_is_running()) {
+            std::this_thread::sleep_for(std::chrono::microseconds(5000));
+        }
+    });
+
+    // run the viewer in the current thread
+    #ifdef USE_PANGOLIN_VIEWER
+    viewer.run();
+    #elif USE_SOCKET_PUBLISHER
+    publisher.run();
+    #endif
+
+    thread.join();
+
+    // shutdown the SLAM process
+    SLAM.shutdown();
+
+    if (!map_db_path.empty()) {
+        // output the map database
+        SLAM.save_map_database(map_db_path, true);
+    }
+
+    std::sort(track_times.begin(), track_times.end());
+    const auto total_track_time = std::accumulate(track_times.begin(), track_times.end(), 0.0);
+    std::cout << "median tracking time: " << track_times.at(track_times.size() / 2) << "[s]" << std::endl;
+    std::cout << "mean tracking time: " << total_track_time / track_times.size() << "[s]" << std::endl;
+
+}
+
 void rgbd_tracking(const std::shared_ptr<openvslam::config>& cfg,
                    const std::string& vocab_file_path,
                    const float scale, const std::string& map_db_path) {
@@ -218,6 +461,10 @@ int main(int argc, char* argv[]) {
     // run tracking
     if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::RGBD) {
         rgbd_tracking(cfg, vocab_file_path->value(), scale->value(), map_db_path->value());
+    } else if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Monocular) {
+        mono_tracking(cfg, vocab_file_path->value(), scale->value(), map_db_path->value());
+    } else if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Stereo) {
+        stereo_tracking(cfg, vocab_file_path->value(), scale->value(), map_db_path->value());
     }
     else {
         throw std::runtime_error("Invalid setup type: " + cfg->camera_->get_setup_type_string());
